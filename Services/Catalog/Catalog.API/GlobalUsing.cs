56e9f96e8b213f9724a8267df58aef92076d8edc
PangSoramDepo <pangsoramdepo@gmail.com>
2024-07-06 06:23:10 +0700

    feat: api get products

--- Services/Catalog/Catalog.API/GlobalUsing.cs
+++ Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -4,3 +4,5 @@ global using MediatR;
 global using Marten;
 global using FluentValidation;
 global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.CQRS;
+global using Catalog.API.Models;
---
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndPoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async ([AsParameters] GetProductsRequest requst, ISender sender) =>
+            {
+                var command = requst.Adapt<GetProductsQuery>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("GetProducts")
+                .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Products")
+                .WithDescription("Get Products");
+        }
+    }
+}
