890e75c277e1b8a0c2b03489a6f3d2bca6ca8687
PangSoramDepo <pangsoramdepo@gmail.com>
2024-07-07 13:02:49 +0700

    feat: api update product by id

---
+++ Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndPoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductRequest(Guid Id, string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
+    public record UpdateProductResponse(bool IsSuccess);
+    public class UpdateProductEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products/{id}", async (Guid Id, UpdateProductRequest request, ISender sender) =>
+            {
+                var requestWithId = request with { Id = Id };
+                var command = requestWithId.Adapt<UpdateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateProductResponse>();
+                return Results.Ok(response);
+            })
+                .WithName("UpdateProduct")
+                .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithSummary("Update Product")
+                .WithDescription("Update Product");
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Catalog.API.Products.GetProductById;
+
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool IsSuccess);
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().WithMessage("Product ID is required");
+            RuleFor(command => command.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+            RuleFor(command => command.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+    public class UpdateProductHandler(IDocumentSession session, ISender sender) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var getProductById = await sender.Send(new GetProductByIdQuery(request.Id));
+            var product = getProductById.Product;
+            product.Name = request.Name;
+            product.Price = request.Price;
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new UpdateProductResult(true);
+        }
+    }
+}
