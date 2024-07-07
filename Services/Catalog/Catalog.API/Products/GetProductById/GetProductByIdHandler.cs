fcfa6f1ebc291163267c9e4f207c6c7ba070ee39
PangSoramDepo <pangsoramdepo@gmail.com>
2024-07-07 10:40:02 +0700

    feat: api get product by id

---
+++ Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    public class GetProductByIdHandler(IDocumentSession session) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await session.LoadAsync<Product>(request.Id, cancellationToken);
+            if (result is null)
+            {
+                throw new ProductNotFoundException(request.Id);
+            }
+            return new GetProductByIdResult(result);
+        }
+    }
+}
