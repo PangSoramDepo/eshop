1f17704d6858938d2b5d9d8a10dc45bb8f5e1bae
PangSoramDepo <pangsoramdepo@gmail.com>
2024-07-13 00:22:37 +0700

    feat: init basket solution with some config di and app

---
+++ Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using Carter;
+global using MediatR;
+global using Marten;
+global using BuildingBlocks.Behaviors;
+global using HealthChecks.UI.Client;
+global using BuildingBlocks.Exceptions.Handler;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
---
+++ Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,32 @@
+//CONFIG DEPENDENCY INJECTION
+var assembly = typeof(Program).Assembly;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+//CROSS CUTTING CONCERN
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
+
+//CONFIG APP
+var app = builder.Build();
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    }
+);
+app.Run();
